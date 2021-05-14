bb4cd2ce871bdfc8670a0268c1dda80d1b71a945
adam_stefaniak <lensoisdunord62110>
2021-05-14 19:10:56 +0200

    File reader and parse initiation

--- app.js
+++ app.js
@@ -16,6 +16,12 @@
 
 // prendre en compte l'erreur lors d'une commande mal formée
 
+// écrire la documentation d'installation / utilisation
+
+const dataFile = require('./data');
+const dataToRead = dataFile.data;
+
+	// We verify the number of arguments given 
 	if (process.argv.length != 3){
 		console.error('Invalid number of parameters. Expected 1 but got '+(process.argv.length-2));
 	}
@@ -28,19 +34,26 @@
 		command = processArg.split('=')[0];
 		commandOption = processArg.split('=')[1];
 		
+		/*
 		console.log("command: "+command);
 		console.log("option: "+commandOption);
+		*/
 		
 		// remplacer par un switch case
 		if (command === "filter"){
 			
 			// faire un test sur l'option
-			filterCommand();
+			
+			// on affiche uniquement les 'animals' qui contiennent le pattern (ici l'option de la commande)
+			console.log(dataToRead);
+			var jsonObj = JSON.parse(dataToRead);
+			console.log(jsonObj.name);
+			
+			// https://www.w3schools.com/js/js_json_parse.asp
+			
 		}
 		else if (command === "count"){
-			
-			// faire un test sur l'option
-			countCommand();
+			console.log(dataToRead);
 		}
 		else{
 			console.error('Invalid command, please try again.');
@@ -50,6 +63,8 @@
 	
 	function filterCommand(){
 		console.log('filter à compléter en pensant à prendre en compte loption');
+		// ouverture et parcours du fichier
+		
 	}
 	
 	function countCommand(){
