8ba90522a68aaf94ac1c0e5b1b06009500bf2981
adam_stefaniak <lensoisdunord62110>
2021-05-18 23:56:30 +0200

    Adding results for filter method

--- app.js
+++ app.js
@@ -1,79 +1,119 @@
-// [A faire]
-
-// faire une énumération avec toutes les commandes valides ?
-
-// appeler la bonne méthode pour le traitement désiré
-
-// première commande : --filter=ry
-
-// seconde méthode : --count
-
-// lire le fichier 'data.js'
-
-// mettre tout dans une fonction que l'on appelle en fin de fichier ?
-
-// penser à adapter le code pour d'autres commandes (penser à avoir un code évolutif)
-
-// prendre en compte l'erreur lors d'une commande mal formée
-
 // écrire la documentation d'installation / utilisation
+// commenter et renommer les fonctions en anglais
 
-const dataFile = require('./data');
+const dataFile = require('./data.js');
 const dataToRead = dataFile.data;
 
-	// We verify the number of arguments given 
+function interprate(){
+	// Number of arguments verification
 	if (process.argv.length != 3){
 		console.error('Invalid number of parameters. Expected 1 but got '+(process.argv.length-2));
 	}
+	// Command composition verification
+	else if (!process.argv[2].includes("--")){
+		console.error('Invalid command, please try again.');
+	}
 	else{
 		let processArg = process.argv[2];
-		let command;
-		let commandOption;
+		let command = null;
+		let commandOption = null;
 		
 		processArg = processArg.split('--')[1];
 		command = processArg.split('=')[0];
-		commandOption = processArg.split('=')[1];
 		
-		/*
-		console.log("command: "+command);
-		console.log("option: "+commandOption);
-		*/
-		
-		// remplacer par un switch case
-		if (command === "filter"){
+		switch (command){
 			
-			// faire un test sur l'option
+			case 'filter':
+				// Option verification
+				if (!process.argv[2].includes("=")){
+					console.error('Invalid command, please try again.');
+					break;
+				}
+				
+				commandOption = processArg.split('=')[1];
+				
+				if (commandOption.length == 0){
+					console.error('Invalid command, please try again.');
+					break;
+				}
+				else{
+					filterCommand(commandOption);
+				}
 			
-			// on affiche uniquement les 'animals' qui contiennent le pattern (ici l'option de la commande)
-			console.log(dataToRead);
-			var jsonObj = JSON.parse(dataToRead);
-			console.log(jsonObj.name);
-			
-			// https://www.w3schools.com/js/js_json_parse.asp
-			
+				break;
+				
+			case 'count':
+				if (process.argv[2].includes("=")){
+					console.error('Invalid command, please try again.');
+					break;
+				}
+				countCommand();
+				break;
+				
+			default:
+				console.error('Invalid command, please try again.');
+				break;
 		}
-		else if (command === "count"){
-			console.log(dataToRead);
-		}
-		else{
-			console.error('Invalid command, please try again.');
-		}
-		
 	}
-	
-	function filterCommand(){
-		console.log('filter à compléter en pensant à prendre en compte loption');
-		// ouverture et parcours du fichier
-		
-	}
-	
-	function countCommand(){
-		console.log('count à compléter en pensant à prendre en compte loption');
-	}
+}
 
-/*
-process.argv.forEach(function (val, index, array) {
-  //console.log(index + ': ' + val);
-  console.log(array.length);
-});
-*/
+function filterCommand(commandOption){
+	
+	let filteredData = {};
+	let values = Object.values(dataToRead);
+	let values2 = null;
+	let values3 = null;
+	let animals = {};
+	let people = {};
+	var i, j, k;
+	
+	// optimiser la complexité
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+	for(i = 0; i < values.length; i++){
+		
+		values2 = Object.values(values[i].people);
+		
+		for(j = 0; j < values2.length ; j++){
+			
+			values3 = Object.values(values2[j].animals);
+			
+			for (k = 0; k < values3.length; k++){
+				
+				if (values3[k].name.includes(commandOption)){
+					if (!animals['name'].includes(values3[k].name)){
+						
+						animals['name'] = values3[k].name;
+						if (!people['name'].includes(values2[j].name)){
+							people['name'] = values2[j].name;
+							people['animals'] = {};
+						}
+						people['animals'] = animals;
+						animals = {};
+					}
+					filteredData['name'] = values[i].name;
+					filteredData['people'] = people;
+					people = {};
+				}
+			}
+		}
+	}
+	
+	console.log(filteredData);
+	
+}
+
+// sous-fonction booléenne qui permet 
+
+function countCommand(){
+	
+	let countedData = {};
+	
+	
+	console.log('count à compléter en pensant à prendre en compte loption');
+	// ouverture et parcours du fichier
+	
+	console.log();
+}
+
+// Main method call
+interprate();
